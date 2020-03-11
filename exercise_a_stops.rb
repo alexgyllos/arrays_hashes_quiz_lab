080aa300df17efd727c28a4cd2e4d4270ada2d46
Alex Gyllos <alex.gyllos@gmail.com>
2020-03-11 17:03:40 +0000

    prints out all the stops using a for loop

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -20,5 +20,7 @@ p stops[2]
 #9. Reverse the positions of the stops in the array
 p stops.reverse
 #10 Print out all the stops using a for loop
-
-p stops
+for stop in stops
+  p stop
+end
+#p stops
